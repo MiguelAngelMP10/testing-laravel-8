c3e36feb0e93899d8002aff4f8f9640bd583165e
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-02 09:18:24 -0500

    Accessors y Mutators

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower($value); // Mutators
+    }
+
+    public function getSlugAttribute()
+    {
+        return str_replace(' ', '-', $this->name); // Accessors
+    }
+}
---
+++ tests/Unit/Models/PostTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Post;
+
+class PostTest extends TestCase
+{
+    public function test_set_name_in_lowercase()
+    {
+        $post = new Post;
+        $post->name = 'Proyecto en PHP';
+
+        $this->assertEquals('proyecto en php', $post->name);
+    }
+
+    public function test_get_slug()
+    {
+        $post = new Post;
+        $post->name = 'Proyecto en PHP';
+
+        $this->assertEquals('proyecto-en-php', $post->slug);
+    }
+}
